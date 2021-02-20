676a7bd6fe62e4a7082fb0540b4fc8bdd4bc51a8
James Noonan <james@noonanfamily.org>
2021-02-21 09:50:15 +1100

    Hamburger Menu
    Show a hamburger menu and side drawer on smaller screens for navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,53 @@
 // The navbar which links to different sections of the website
+import React, { useState } from 'react';
 
 import logo from '../assets/logo.png';
 import heart from '../assets/icons/heart.svg';
 import compare from '../assets/icons/compare.svg';
 
 function Navbar(props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav className="w-full h-28 px-5 lg:px-20 bg-blue-600">
-      <div className="h-full flex justify-between items-center">
-        <div className="pr-2">
-          <img src={logo} alt="Reality Properties Logo" />
+    <>
+      <nav className="relative z-50 w-full h-28 px-5 lg:px-20 shadow-md bg-blue-600">
+        <div className="h-full flex justify-between items-center">
+          <div className="pr-2">
+            <img src={logo} alt="Reality Properties Logo" />
+          </div>
+          <ul className="h-full w-4/5 lg:w-1/2 mr-auto hidden md:flex justify-around items-center text-white text-xs font-light">
+            <li>Homepage</li>
+            <li>About Us</li>
+            <li>For Rent</li>
+            <li>For Sale</li>
+            <li>Our Agents</li>
+            <li>Contact Us</li>
+          </ul>
+          <ul className="hidden md:flex items-center">
+            <li>
+              <img className="p-3" src={compare} alt="Compare" />
+            </li>
+            <li>
+              <img className="p-3" src={heart} alt="Loved" />
+            </li>
+          </ul>
+          <div
+            id="hamburger-wrapper"
+            className="py-6 px-2 cursor-pointer"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <div
+              id="hamburger"
+              className={`md:hidden ${menuOpen ? 'close' : ''}`}
+            ></div>
+          </div>
         </div>
-        <ul className="h-full w-4/5 lg:w-1/2 mr-auto hidden md:flex justify-around items-center text-white text-xs font-light">
+      </nav>
+      <div
+        id="side-menu"
+        className={`fixed md:hidden shadow-2xl ${menuOpen ? 'open' : ''}`}
+      >
+        <ul className="flex flex-col items-center text-white text-lg leading-10 font-light">
           <li>Homepage</li>
           <li>About Us</li>
           <li>For Rent</li>
@@ -19,7 +55,7 @@ function Navbar(props) {
           <li>Our Agents</li>
           <li>Contact Us</li>
         </ul>
-        <ul className="hidden md:flex items-center">
+        <ul className="flex justify-center items-center">
           <li>
             <img className="p-3" src={compare} alt="Compare" />
           </li>
@@ -28,7 +64,7 @@ function Navbar(props) {
           </li>
         </ul>
       </div>
-    </nav>
+    </>
   );
 }
 
